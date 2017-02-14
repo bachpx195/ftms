c4087ea0e84faccd8ec14a5981144ae753e25615
dat-hedspi <dat.hedspi@gmail.com>
2017-02-14 08:29:51 +0700

    fix structure react

--- client/app/bundles/startup/registration.jsx
+++ client/app/bundles/startup/registration.jsx
@@ -1,8 +1,8 @@
 import ReactOnRails from 'react-on-rails';
 
-import Header from '../components/header';
-import Sidebar from '../components/sidebar';
-import Footer from '../components/footer';
+import Header from '../components/dashboards/header';
+import Sidebar from '../components/dashboards/sidebar';
+import Footer from '../components/dashboards/footer';
 
 // This is how react_on_rails can see the HelloWorld in the browser.
 ReactOnRails.register({
