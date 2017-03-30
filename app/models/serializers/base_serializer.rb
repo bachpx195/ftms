7ef4f83200c845cfb67da6ada91fca0895720f01
nguyenhuutien <nguyen.huu.tien@framgia.com>
2017-03-30 09:06:24 +0700

    Fix warning serializer

--- app/models/serializers/base_serializer.rb
+++ app/models/serializers/base_serializer.rb
@@ -7,7 +7,7 @@ class Serializers::BaseSerializer
   end
 
   def serializer
-    serializers = if class_is_relation?
+    if class_is_relation?
       multi_serializer
     elsif @class_name.present? && self.class.to_s != @class_name
       object_serializer = @class_name.constantize.new object: @object,
@@ -21,9 +21,9 @@ class Serializers::BaseSerializer
   def multi_serializer
     return [] if @object.empty?
     class_name = get_class_name.constantize
-    serializers = @object.collect do |object|
+    @object.map do |object|
       object_serializer = class_name.new object: object, scope: @scope
-      serializer = object_serializer.serializer
+      object_serializer.serializer
     end
   end
 
@@ -31,60 +31,63 @@ class Serializers::BaseSerializer
     serializers = Hash.new
     self.attributes.each do |attribute|
       instance_variable_set "@#{attribute}", @object.try(attribute)
+      if attribute.in? self.methods
+        serializers[attribute.to_sym] = self.send attribute
+      end
     end
-    self.attributes.each{|method|
-      serializers[method.to_sym] = self.send method if method.in? self.methods}
     serializers
   end
 
   def attributes
-    attributes = Array.new
-    superclass ||= @class_name.constantize if @class_name.present?
-    superclass ||= self.class
-    while superclass != Serializers::BaseSerializer do
-      attributes += superclass.attributes
-      superclass = superclass.superclass
-    end
+    attributes = get_attributes
     self.class.conditions.each do |key, value|
       attributes.delete key unless self.send(value)
     end
     attributes
   end
 
+  def get_attributes
+    superclass = get_class
+    attributes = Array.new
+    while superclass != Serializers::BaseSerializer
+      attributes += superclass.attributes
+      superclass = superclass.superclass
+    end
+    attributes
+  end
+
+  def get_class
+    superclass ||= @class_name.constantize if @class_name.present?
+    superclass || self.class
+  end
+
   def class_is_relation?
-    superclass = @object.class.superclass
+    class_object = @object.class
+    superclass = class_object.superclass
     superclass == ActiveRecord::Relation || superclass.superclass ==
-      ActiveRecord::Relation || @object.class == Array
+      ActiveRecord::Relation || class_object == Array
   end
 
   def get_class_name
     return @class_name if @class_name.present?
-    class_name = if self.class == Serializers::BaseSerializer
+    class_self = self.class
+    if class_self == Serializers::BaseSerializer
       sub_class = get_super_class @object.first.class
       "Serializers::#{sub_class.to_s.pluralize}Serializer"
+    else
+      class_self.to_s
     end
-    class_name ||= self.class.to_s
   end
 
-  def get_super_class object_class
-    sub_class = object_class
-    get_super_class object_class.superclass if object_class != ApplicationRecord
-    object_class
+  def get_super_class obj_class
+    get_super_class obj_class.superclass if obj_class != ApplicationRecord
+    obj_class
   end
 
   class << self
     def attr_accessor *vars
-      @attributes ||= Array.new
-      @conditions ||= Hash.new
-      attr_var = Array.new
-      vars.each_with_index.map do |var, i|
-        if vars[i + 1] && vars[i + 1].class == Hash
-          @conditions["#{vars[i]}".to_sym] = vars[i + 1][:if]
-          attr_var.push vars[i + 1]
-        end
-      end
-      vars -= attr_var
-      @attributes.concat vars
+      vars -= attributes_with_condition vars
+      attributes.concat vars
       super *vars
     end
 
@@ -94,6 +97,23 @@ class Serializers::BaseSerializer
 
     def conditions
       @conditions ||= Hash.new
+    end
+
+    private
+    def attributes_with_condition vars
+      vars.each_with_index.map do |var, index|
+        var_next = vars[index + 1]
+        is_condition?(var, var_next) ? var_next : nil
+      end
+    end
+
+    def is_condition? var, var_next
+      if var_next && var_next.class == Hash
+        conditions[var.to_s.to_sym] = var_next[:if]
+        true
+      else
+        false
+      end
     end
   end
 end
