e7a84ff1a333f60b49a85809bd98921bab9539da
Hang Xuka <phamhangmta94@gmail.com>
2017-04-04 09:58:16 +0700

    Test_4

--- features/step_definitions/manage_organization_steps.rb
+++ features/step_definitions/manage_organization_steps.rb
@@ -1,14 +1,20 @@
-Given(/^User logged with email is "([^"]*)"$/) do |arg1|
+Given(/^User logged in with email "([^"]*)" and password "([^"]*)"$/) do |email, password|
   pending # Write code here that turns the phrase above into concrete actions
 end
 
-Given(/^User has functions are controller is "([^"]*)", action is "([^"]*)"\.$/) do |arg1, arg2|
+Given(/^User manage organization include create, update, delete\.$/) do
   pending # Write code here that turns the phrase above into concrete actions
 end
 
-# Create organization
+Given(/^In this organization, user manage program include create, update, delete\.$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
 
-Given(/^Input organization name is "([^"]*)"\.$/) do |arg1|
+Given(/^User has role create organization\.$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Given(/^Input organization name "([^"]*)"\.$/) do |name|
   pending # Write code here that turns the phrase above into concrete actions
 end
 
@@ -16,13 +22,63 @@ When(/^In the Organization screen\. User click button Save\.$/) do
   pending # Write code here that turns the phrase above into concrete actions
 end
 
-Then(/^I should redirect to show "([^"]*)" screen\.$/) do |arg1|
+Then(/^I should redirect to organization name "([^"]*)" in show organization screen\.$/) do |name|
   pending # Write code here that turns the phrase above into concrete actions
 end
 
-# Edit organization
+Given(/^In this organization, user has role create program$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
 
-Given(/^Edit Organization with name is "([^"]*)" to name is "([^"]*)"$/) do |arg1, arg2|
+Given(/^Insert program name "([^"]*)" for organization\.$/) do |program|
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+When(/^In the show organization screen, user click button Save\.$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then(/^I should redirect to program name "([^"]*)" in show organization screen$/) do |program|
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Given(/^In this organization, user has role update program$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Given(/^Update program with name "([^"]*)" to "([^"]*)" into organization\.$/) do |old_program, new_program|
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+When(/^User click button Save$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then(/^I should redirect to program with name "([^"]*)" in show organization screen$/) do |new_program|
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Given(/^User want to delete program name "([^"]*)"$/) do |program|
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Given(/^In this organization, user has role delete program$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+When(/^User click delete symbol\.$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then(/^Program name "([^"]*)" deleted and should redirect to show organization screen$/) do |program|
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Given(/^User can update organization$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Given(/^Edit organization with name "([^"]*)" to name "([^"]*)"$/) do |old_name, new_name|
   pending # Write code here that turns the phrase above into concrete actions
 end
 
@@ -30,28 +86,70 @@ When(/^In the Organization screen, user click to submit organization form$/) do
   pending # Write code here that turns the phrase above into concrete actions
 end
 
-Then(/^I should redirect to organization name "([^"]*)" in show organization screen\.$/) do |arg1|
+Then(/^I should redirect to organization name "([^"]*)" in show organization screen$/) do |new_name|
   pending # Write code here that turns the phrase above into concrete actions
 end
 
-# Delete organization
-
-Given(/^User want to delete orgnization name is "([^"]*)"$/) do |arg1|
+Given(/^In this organization updated, user can create program$/) do
   pending # Write code here that turns the phrase above into concrete actions
 end
 
-When(/^In the list organization screen\. User click to delete symbol$/) do
+Given(/^Insert program with name "([^"]*)" into organization updated$/) do |program|
   pending # Write code here that turns the phrase above into concrete actions
 end
 
-When(/^Screen display confirm form$/) do
+When(/^In the show organization screen, user click button Save$/) do
   pending # Write code here that turns the phrase above into concrete actions
 end
 
-When(/^user click OK$/) do
+Then(/^I should redirect to program with name "([^"]*)" in show organization updated screen\.$/) do |program|
   pending # Write code here that turns the phrase above into concrete actions
 end
 
-Then(/^Organization name "([^"]*)" deleted and should redirect to list organization screen$/) do |arg1|
+Given(/^In this organization updated, user can update program$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Given(/^Update program with name "([^"]*)" to "([^"]*)" of organization updated\.$/) do |old_program, new_program|
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+When(/^In the Organization screen\. User click button Save$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then(/^I should redirect to program with name "([^"]*)" in show organization updated screen$/) do |new_program|
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Given(/^In this organization updated, user can delete program$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Given(/^User want to delete program with name "([^"]*)"$/) do |program|
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+When(/^User click to delete symbol$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then(/^Program with name "([^"]*)" deleted and should redirect to show organization screen$/) do |program|
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Given(/^User can delete organization$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Given(/^User want to delete orgnization with name "([^"]*)"$/) do |name|
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+When(/^In the list organization screen\. User click to delete symbol\.$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then(/^Organization with name "([^"]*)" deleted and should redirect to list organization screen\.$/) do |name|
   pending # Write code here that turns the phrase above into concrete actions
 end
