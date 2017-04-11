859556531ac0ddb6c2d894476c455f421cbe39e3
HoangLanh <hoanglanhtb@gmail.com>
2017-04-11 11:16:06 +0700

    manage university

---
+++ features/step_definitions/manage_universities_steps.rb
@@ -0,0 +1,83 @@
+Given(/^user has permission manage Universities include show all Universities, create, edit, delete$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Given(/^system existed Universities with name is "([^"]*)"$/) do |university_name|
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Given(/^user has a permission to create university$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+When(/^user click button Save in the new university screen$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then(/^User should redirect to list Universities screen and name "([^"]*)" in show Universities screen$/) do |university_name|
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then(/^User should redirect to list Universities screen message is "([^"]*)"$/) do |message|
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Given(/^user has a permission to show all Universities$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Given(/^system has (\d+) Universities above$/) do |arg1|
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+When(/^Universities screen opened$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Given(/^system existed university with name is "([^"]*)"$/) do |university_name|
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then(/^System display (\d+) Universities on form and has dropdown for paginate$/) do |arg1|
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Given(/^user has a permission to edit university$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Given(/^edit university with nane is "([^"]*)" to name is "([^"]*)"$/) do |old_university_name, new_university_name|
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+When(/^user click button Save in the edit university screen$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Given(/^edit university with name is "([^"]*)" to name is "([^"]*)"$/) do |old_university_name, new_university_name|
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Given(/^edit university with name is "([^"]*)"$/) do |university_name|
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then(/^should redirect to list Universities screen and message is "([^"]*)"$/) do |message|
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Given(/^user has a permission to delete university$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Given(/^user delete university name is "([^"]*)"$/) do |university_name|
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+When(/^User click to delete symbol in the list Universities screen$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then(/^should redirect to list Universities screen and university with name is "([^"]*)" removed$/) do |university_name|
+  pending # Write code here that turns the phrase above into concrete actions
+end
