81f3111aa6a6045d5f077d719a5a7434ffc08835
nhocbangchu <duchv2307@gmail.com>
2017-04-20 21:38:10 +0700

    Remove some file organization

--- client/app/bundles/components/show_organizations/sub_organization_constants.jsx
+++
@@ -1,3 +0,0 @@
-import * as app_constants from 'constants/app_constants';
-export const SUB_ORGANIZATION_PATH = app_constants.APP_NAME + 'sub_organizations';
-export const ORGANIZATION_PATH = app_constants.APP_NAME + 'organizations';
