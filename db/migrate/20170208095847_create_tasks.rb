fb6ae89c93e20b790384dd9c74fc1d26bd96a1e3
Duong <yeunuthongminh@gmail.com>
2017-04-28 08:11:07 +0700

    fix db

--- db/migrate/20170208095847_create_tasks.rb
+++ db/migrate/20170208095847_create_tasks.rb
@@ -5,7 +5,8 @@ class CreateTasks < ActiveRecord::Migration[5.0]
       t.string :targetable_type
       t.integer :ownerable_id
       t.string :ownerable_type
-      t.integer :user_id
+      t.integer :objectable_id
+      t.string :objectable_type
       t.integer :status
       t.string :type
       t.datetime :deleted_at
