b2dddb88276d4f3d79056f1076ba6d5a95438249
dungvuit <dungvuit@gmail.com>
2017-05-08 09:43:01 +0700

    Apply new table to projects

--- client/app/bundles/components/projects/projects.jsx
+++ client/app/bundles/components/projects/projects.jsx
@@ -1,10 +1,10 @@
-import React from 'react';
-import axios from 'axios';
-import Griddle, {plugins, RowDefinition, ColumnDefinition} from 'griddle-react';
-import { NewLayout } from '../shareds/griddles/new_layout';
-import Form from './templates/form';
-import * as table_constants from 'constants/griddle_table_constants';
+import * as react_table_ultis from 'shared/react-table/ultis';
 import * as routes from 'config/routes';
+import * as table_constants from 'constants/griddle_table_constants';
+import css from 'assets/sass/react-table.scss';
+import Form from './templates/form';
+import React from 'react';
+import ReactTable from 'react-table';
 
 export default class Projects extends React.Component {
   constructor(props) {
@@ -16,41 +16,52 @@ export default class Projects extends React.Component {
   }
 
   render() {
-    {NewLayout}
-
-    const LinkToProject = ({value, griddleKey}) => {
-      let project = this.state.projects[griddleKey];
-      let project_url = '#';
-      if (project) {
-        project_url = routes.project_url(project.id);
-      }
-      return <a href={project_url}>{value}</a>;
-    };
-
-    const OrganizationName = ({value, griddleKey}) => {
-      let project = this.state.projects[griddleKey];
-      let name = '';
-      if (project.organization) {
-        name = project.organization.name;
-      }
-      return <span>{name}</span>;
-    };
+    const columns = [
+      {
+        header: '#',
+        accessor: 'position',
+        render: row => <div className='text-right'>{row.index + 1}</div>,
+        hideFilter: true,
+        width: 50
+      },
+      {
+        header: I18n.t('projects.headers.name'),
+        accessor: 'name',
+        render: row => {
+          let project = this.state.projects[row.value];
+          let project_url = '#';
+          if (project) {
+            project_url = routes.project_url(project.id);
+          }
+          return <a href={routes.project_url(row.row.id)}>{row.value}</a>;
+        }
+      },
+      {
+        header: I18n.t('projects.organization'),
+        accessor: 'organization',
+        render: row => {
+          let project = this.state.projects[row.index];
+          let name = '';
+          if (project.organization) {
+            name = project.organization.name;
+          }
+          return <span>{name}</span>;
+        }
+      },
+      {
+        header: I18n.t('projects.headers.description'),
+        accessor: 'description',
+      },
+    ]
 
     return (
       <div>
-        <Griddle data={this.state.projects} plugins={[plugins.LocalPlugin]}
-          components={{Layout: NewLayout}}
-          styleConfig={table_constants.styleConfig}>
-          <RowDefinition>
-            <ColumnDefinition id='name' title={I18n.t('projects.headers.name')}
-              customComponent={LinkToProject} />
-            <ColumnDefinition id='organization'
-              title={I18n.t('projects.organization')}
-              customComponent={OrganizationName} />
-            <ColumnDefinition id='description'
-              title={I18n.t('projects.headers.description')} />
-          </RowDefinition>
-        </Griddle>
+        <ReactTable
+          className='-striped -highlight' data={this.state.projects}
+          columns={columns} showFilters={true}
+          defaultPageSize={react_table_ultis.defaultPageSize}
+          defaultFilterMethod={react_table_ultis.defaultFilter}
+        />
       </div>
     );
   }
