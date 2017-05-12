0a4d90c2c7c5f8bd1a5ed6aa4a8aa8a354dd1b5a
dat-hedspi <dat.hedspi@gmail.com>
2017-05-12 14:41:40 +0700

    fix-user_join-2-course

--- app/models/serializers/courses/course_members_serializer.rb
+++ app/models/serializers/courses/course_members_serializer.rb
@@ -1,6 +1,6 @@
 class Serializers::Courses::CourseMembersSerializer <
   Serializers::SupportSerializer
-  attrs :id, :name, :avatar
+  attrs :id, :name, :avatar, :other_courses
   attrs :user_course, if: :check_course
 
   def avatar
@@ -8,7 +8,11 @@ class Serializers::Courses::CourseMembersSerializer <
   end
 
   def user_course
-    object.user_courses.find_by course_id: course.id
+    object.user_courses.find_by course_id: course.id || []
+  end
+
+  def other_courses
+    object.member_courses.where(status: :init || :in_progress) || []
   end
 
   private
